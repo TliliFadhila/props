b837fd76a73eb102ed1cbcfb17d78037890cf18c
TliliFadhila <ziiwii40@gmail.com>
2021-05-21 02:28:29 +0100

    new

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import Profile from "./components/Profile";
+import pic from './components/me.jpg'
+
+
+
+function App() {
+  
+  const fullName = "Fadhila Tlili";
+  
+  
+  const bio = `25 years old,from Sidi Bouzid Sfax`;
+  
+  
+  
+  const profession = " PhD Student and Graphics Designer";
+
+
+const show = (name) => {
+  alert(name)
+}
+  return (
+    <div className="App">
+      <Profile name={fullName} bio={bio} profession={profession} show={show}>
+      <img className="str" src={pic} alt='Bellatrix'/> 
+      </Profile>
+      
+    </div>
+  );
+}
+
+export default App;
